024d1ce6bba9c49889d2bac55a0568cb40c312da
Tkachenko_Yuriy <uvt86@ukr.net>
2024-03-06 22:53:48 +0200

    add Chronicles & Documents

--- src/components/Documents/DocumentsList/DocumentsList.jsx
+++ src/components/Documents/DocumentsList/DocumentsList.jsx
@@ -1,15 +1,24 @@
+import DocumentsItem from '../DocumentsItem/DocumentsItem';
+import css from './DocumentsList.module.scss';
+
 const DocumentsList = ({ items }) => {
-  // return (
-  //   <ul>
-  //     {items.map(({ id, title, description, link }) => (
-  //       <li key={id}>
-  //         <h3>{title}</h3>
-  //         <p>{description}</p>
-  //         <p>{link}</p>
-  //       </li>
-  //     ))}
-  //   </ul>
-  // );
+  return (
+    <>
+      <h3 className={css.title}>Документи</h3>
+      <ul className={css.list}>
+        {items.map(({ id, title, subtitle, description, link }) => (
+          <li key={id} className={css.item}>
+            <DocumentsItem
+              title={title}
+              subtitle={subtitle}
+              description={description}
+              link={link}
+            />
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 export default DocumentsList;
