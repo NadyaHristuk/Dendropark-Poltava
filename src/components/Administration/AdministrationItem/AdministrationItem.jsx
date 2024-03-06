5ca6d0d9566edf9fa5d2c325895584e9d9a69738
Dmytro Liuba <lyubadmitry2@gmail.com>
2024-03-07 00:29:48 +0200

    Update AdministrationItem.jsx

--- src/components/Administration/AdministrationItem/AdministrationItem.jsx
+++ src/components/Administration/AdministrationItem/AdministrationItem.jsx
@@ -1,9 +1,10 @@
-const AdministrationItem = ({id, name, photo, photoRetina, text}) => {
+// eslint-disable-next-line react/prop-types
+const AdministrationItem = ({ name, photo, photoRetina, text }) => {
   return (
     <li>
-        <img src={photo} alt={name} />
-        <p>{name}</p>
-        <p>{text}</p>
+      <img src={photo} alt={name} srcSet={`${photo} 1x, ${photoRetina} 2x`} />
+      <p>{name}</p>
+      <p>{text}</p>
     </li>
   );
 };
