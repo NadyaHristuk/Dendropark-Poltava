e220189591e2d55e61d2ab302e2a14f8e8ebe749
Dmytro Liuba <lyubadmitry2@gmail.com>
2024-03-06 13:40:46 +0200

    add Container

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import SharedLayout from './components/SharedLayout/SharedLayout';
 import HomePage from './pages/HomePage';
 import NotificationsProvider from './context/NotificationsProvider';
+import ContactPage from './pages/ContactPage';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<HomePage />} />
           {/* <Route path="about" element={<AboutPage />} /> {/* Додавання сторінки "Про нас" */}
-        {/*   <Route path="contact" element={<ContactPage />} /> Додавання сторінки "Контакти" */}
+          <Route path="contact" element={<ContactPage />} />
 
           <Route path="*" element={<Navigate to={'/'} />} />
         </Route>
