673290dc3f9082fd9f030571d4ae13fd4ee29262
Dmytro Liuba <lyubadmitry2@gmail.com>
2024-03-07 11:00:30 +0200

    update

--- src/components/Administration/data.js
+++ src/components/Administration/data.js
@@ -1,34 +1,36 @@
-import person1 from '../../assets/images/Administration/person-1.jpg';
-import person1Retina from '../../assets/images/Administration/person-1@2x.jpg';
-import person2 from '../../assets/images/Administration/person-2.jpg';
-import person2Retina from '../../assets/images/Administration/person-2@2x.jpg';
-import person3 from '../../assets/images/Administration/person-3.jpg';
-import person3Retina from '../../assets/images/Administration/person-3@2x.jpg';
+import {
+  person1_1x,
+  person1_2x,
+  person2_1x,
+  person2_2x,
+  person3_1x,
+  person3_2x,
+} from '../../assets';
 
 const data = [
   {
     id: 1,
-    photo: person1,
-    photoRetina: person1Retina,
+    photo: person1_1x,
+    photoRetina: person1_2x,
     name: 'Ляшко Ігор Іванович',
     text: 'Директор Комунального підприємства "Декоративні Культури"',
   },
 
   {
     id: 2,
-    photo: person2,
-    photoRetina: person2Retina,
+    photo: person2_1x,
+    photoRetina: person2_2x,
     name: 'Макуха Максим Григорович',
     text: 'Начальник відділу "Полтавський міський парк"',
   },
 
   {
     id: 3,
-    photo: person3,
-    photoRetina: person3Retina,
+    photo: person3_1x,
+    photoRetina: person3_2x,
     name: 'Ринковий Григорій Анатолійович',
     text: 'Майстер відділу "Полтавський міський парк"',
   },
 ];
 
-export default data
+export default data;
