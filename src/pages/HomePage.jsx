21483f6c52369c79be9e1fbc94ae258bc6c2412f
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2024-03-08 01:09:06 +0200

    fix bug

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,26 +1,18 @@
-<<<<<<< HEAD
 import { AttentionVisitors } from '../components/AttentionVisitors/AttentionVisitors';
-
 import s from './Home.module.scss';
-import { useTranslation } from 'react-i18next';
-=======
 import Calendar from '../components/Calendar/Calendar';
 import ChroniclesList from '../components/Chronicles/ChroniclesList/ChroniclesList';
 import Container from '../components/Container/Container';
->>>>>>> ab6033451bc3590e0017a48a1e2d6cf734b65d6d
 
 const HomePage = () => {
   return (
-<<<<<<< HEAD
     <div className={s.container}>
-      <AttentionVisitors></AttentionVisitors>
+      <Container>
+        <AttentionVisitors></AttentionVisitors>
+        <Calendar />
+        <ChroniclesList />
+      </Container>
     </div>
-=======
-    <Container>
-      <Calendar />
-      <ChroniclesList />
-    </Container>
->>>>>>> ab6033451bc3590e0017a48a1e2d6cf734b65d6d
   );
 };
 
