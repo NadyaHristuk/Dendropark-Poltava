8d9fc296bafe7adb551a533fb21f4575221d6661
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2024-03-08 00:29:52 +0200

    AttentionVisitors

--- src/components/AttentionVisitors/AttentionVisitors.jsx
+++ src/components/AttentionVisitors/AttentionVisitors.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { AttentionItem } from './AttentionItem';
 import css from './AttentionVisitors.module.scss';
+import { AttentionItem } from './AttentionItem.jsx';
 
 export const AttentionVisitors = () => {
   const { t } = useTranslation();
-
+  const points = t('attentionVisitors.points', { returnObjects: true });
   return (
     <section className={css.container}>
-      <h2>{t('title')}</h2>
-      <p>{t('attentionVisitors.description')}</p>
-      <ul>{/* {data}.map(({(title, description,moreInfo)})) */}</ul>
-      {/* <AttentionItem /> */}
+      <h2 className={css.title}>{t('attentionVisitors.title')}</h2>
+      <p className={css.paragraph}>{t('attentionVisitors.description')}</p>
+      <ul className={css.list}>
+        {points.map((point, index) => (
+          <AttentionItem
+            key={index}
+            title={point.title}
+            description={point.description}
+            moreInfo={point.moreInfo}
+            link={point.link}
+          />
+        ))}
+      </ul>
     </section>
   );
 };
