1174ccbb64297f82280380cfdcb46dc71a56e845
Tkachenko_Yuriy <uvt86@ukr.net>
2024-03-08 19:39:08 +0200

    update chronicles

--- src/components/Chronicles/ChroniclesList/ChroniclesList.jsx
+++ src/components/Chronicles/ChroniclesList/ChroniclesList.jsx
@@ -1,33 +1,13 @@
 import ChroniclesItem from '../ChroniclesItem/ChroniclesItem';
 import { useTranslation } from 'react-i18next';
+import сhronicles from '../сhronicles'
 import css from './ChroniclesList.module.scss';
-
-const сhronicles = [
-  {
-    id: 1,
-    photo:
-      'https://tamtour.com.ua/local/image/883/003/69262-800x600-cfakepathuman3.jpg',
-    title: 'Назва події №1',
-    description: 'Це опис першої події.',
-  },
-  {
-    id: 2,
-    photo:
-      'https://ua.igotoworld.com/frontend/webcontent/images/tours/2702273_800x600_39758_800x600_dsc_0188a.jpg',
-    title: 'Назва події №2',
-    description: 'Це опис другої події.',
-  },
-  {
-    id: 3,
-    photo:
-      'https://ua.igotoworld.com/frontend/webcontent/images/tours/2702054_800x600_poltava.jpg',
-    title: 'Назва події №3',
-    description: 'Це опис третьої події.',
-  },
-];
 
 const ChroniclesList = () => {
   const { t } = useTranslation();
+
+  сhronicles.sort((a, b) => b.date - a.date);
+  
   return (
     <section className={css.chroniclesSection}>
       <h3 className={css.title}>{t('chronicles.chroniclesTitle')}</h3>
@@ -47,3 +27,4 @@ const ChroniclesList = () => {
 };
 
 export default ChroniclesList;
+
---
+++ src/components/Documents/documents.js
@@ -0,0 +1,34 @@
+const documents = [
+    {
+      id: 1,
+      title: 'І. Постанова про парк.',
+      subtitle: 'Документ 1',
+      description: 'Істориччний документ утворення парку.',
+      link: 'https://drive.google.com/file/d/1KESC3sOkhR6wKpYnuNshu7cTiTcTC3yk/view?usp=drive_link',
+    },
+    {
+      id: 2,
+      title: 'II. План реконструкції 2011 р.',
+      subtitle: 'Документ 2',
+      description:
+        'Не був реалізований. Можна використати як карту високої якості.',
+      link: 'https://drive.google.com/file/d/14pVXusGWXQMQ9KMDkZR9lteQ9hmWvn5x/view?usp=drive_link',
+    },
+    {
+      id: 3,
+      title: 'III. Дендроплан шведський ліс.',
+      subtitle: 'Документ 3',
+      description:
+        'Нанесені всі великі дерева ділянки шведського лісу. Дипломна робота студентки.',
+      link: 'https://drive.google.com/file/d/1DxvBxmhit5-P9eKEeoWjIAFa2eJ6zcKS/view?usp=drive_link',
+    },
+    {
+      id: 4,
+      title: 'IV. Фінансовий звіт про благодійні внески 2024.',
+      subtitle: 'Документ 4',
+      description: 'Опису до релізу не буде.',
+      link: 'Посилання - поки рік не кінчився його немає))',
+    },
+  ];
+
+  export default documents;
