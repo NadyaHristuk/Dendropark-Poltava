98e5f698c4b7f934933de0e99cfe87f386d40aaf
Igor Piliaiev <igorpilaev27@gmail.com>
2024-03-08 18:01:03 +0200

    add new pictures

---
+++ src/assets/images/About/index.js
@@ -0,0 +1,59 @@
+// Для карточки 1
+import card1X1Mobile from './card1_x1_mobile.jpg';
+import card1X2Mobile from './card1_x2_mobile.jpg';
+import card1X1Tablet from './card1_x1_tablet.jpg';
+import card1X2Tablet from './card1_x2_tablet.jpg';
+import card1X1Desktop from './card1_x1_desktop.jpg';
+import card1X2Desktop from './card1_x2_desktop.jpg';
+
+// Для карточки 2
+import card2X1Mobile from './card2_x1_mobile.jpg';
+import card2X2Mobile from './card2_x2_mobile.jpg';
+import card2X1Tablet from './card2_x1_tablet.jpg';
+import card2X2Tablet from './card2_x2_tablet.jpg';
+import card2X1Desktop from './card2_x1_desktop.jpg';
+import card2X2Desktop from './card2_x2_desktop.jpg';
+
+// Для карточки 3
+import card3X1Mobile from './card3_x1_mobile.jpg';
+import card3X2Mobile from './card3_x2_mobile.jpg';
+import card3X1Tablet from './card3_x1_tablet.jpg';
+import card3X2Tablet from './card3_x2_tablet.jpg';
+import card3X1Desktop from './card3_x1_desktop.jpg';
+import card3X2Desktop from './card3_x2_desktop.jpg';
+
+
+import map1X1Mobile from './map1_1x_mobile.jpg'
+import map1X2Mobile from './map1_2x_mobile.jpg'
+import map1X1Tablet from './map1_1x_tablet.jpg'
+import map1X2Tablet from './map1_2x_tablet.jpg'
+import map1X1Desktop from './map1_2x_desktop.jpg'
+import map1X2Desktop from './map1_2x_desktop.jpg'
+
+
+export {
+    map1X1Mobile,
+    map1X2Mobile,
+    map1X1Tablet,
+    map1X2Tablet,
+    map1X1Desktop,
+    map1X2Desktop,
+    card1X2Mobile,
+    card1X1Tablet,
+    card1X2Tablet,
+    card1X1Desktop,
+    card1X2Desktop,
+    card2X1Mobile,
+    card2X2Mobile,
+    card2X1Tablet,
+    card2X2Tablet,
+    card2X1Desktop,
+    card2X2Desktop,
+    card3X1Mobile,
+    card3X2Mobile,
+    card3X1Tablet,
+    card3X2Tablet,
+    card3X1Desktop,
+    card3X2Desktop,
+    card1X1Mobile
+};
