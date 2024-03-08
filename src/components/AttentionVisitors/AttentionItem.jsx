2d04b777c6540298da46de24f0aae536fe1bfd60
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2024-03-08 12:51:20 +0200

    add icons and finish styles for mobile

--- src/components/AttentionVisitors/AttentionItem.jsx
+++ src/components/AttentionVisitors/AttentionItem.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import icons from '../../assets/icons/sprite.svg';
 import css from './AttentionVisitors.module.scss';
 
-export const AttentionItem = ({ title, description, moreInfo, link }) => {
+export const AttentionItem = ({
+  title,
+  description,
+  moreInfo,
+  link,
+  iconId,
+}) => {
   return (
     <li className={css.item}>
-      <div className={css.iconBox}></div>
+      <svg width="44" height="44">
+        <use href={`${icons}${iconId}`}></use>
+      </svg>
       <p className={css.point}>{title}</p>
       <p className={css.description}>{description}</p>
       <Link to={link} className={css.moreInfo}>
         {moreInfo}
-        {/* <svg className={css.iconArrow}>
-          <use className={css.use} xlinkHref={'#icon-arrow-up'}></use>
-        </svg> */}
+        <svg width="25" height="25" className={css.iconArrow}>
+          <use href={`${icons}#icon-arrow-up`}></use>
+        </svg>
       </Link>
     </li>
   );
