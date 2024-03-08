da24fc059d593961c5a5e63f52d2c6a17bbe824b
Dmytro Liuba <lyubadmitry2@gmail.com>
2024-03-08 02:55:17 +0200

    Added not found mobile

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import SharedLayout from './components/SharedLayout/SharedLayout';
 import HomePage from './pages/HomePage';
 import ChroniclesPage from './pages/ChroniclesPage';
@@ -6,6 +6,7 @@ import NotificationsProvider from './context/NotificationsProvider';
 import VzaemodiaPage from './components/Vzaemodia/VzaemodiaPage';
 import ContactPage from './pages/ContactPage';
 import AboutPage from './pages/AboutPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 function App() {
   return (
@@ -17,7 +18,7 @@ function App() {
           <Route path="chronicles" element={<ChroniclesPage />} />
           <Route path="about" element={<AboutPage />} />
           <Route path="contact" element={<ContactPage />} />
-          <Route path="*" element={<Navigate to={'/'} />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Route>
       </Routes>
     </NotificationsProvider>
