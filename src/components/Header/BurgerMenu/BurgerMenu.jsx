99cdd70f6414c959a3e00106aef7f7d105e43b71
BestStepInUa <beststep.in.ua@gmail.com>
2024-03-11 16:57:22 +0200

    burger added

--- src/components/Header/BurgerMenu/BurgerMenu.jsx
+++ src/components/Header/BurgerMenu/BurgerMenu.jsx
@@ -16,37 +16,39 @@ const BurgerMenu = () => {
   return (
     <div>
       <button onClick={toggleMenu}>☰</button>
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/" end>
-              {t('header.navigation.main')}
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/about">{t('header.navigation.about')}</NavLink>
-          </li>
-          <li>
-            <NavLink to="/vzaemodia">
-              {t('header.navigation.interaction')}
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/chronicles">
-              {t('header.navigation.materials')}
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to="/service">{t('header.navigation.services')}</NavLink>
-          </li>
-          <li>
-            <NavLink to="/contact">{t('header.navigation.contacts')}</NavLink>
-          </li>
-          {isMobile && (
-            <li>{<a href="tel:+380502894133">+38 (050) 289-41-33</a>}</li>
-          )}
-        </ul>
-      </nav>
+      {isOpen && (
+        <nav>
+          <ul>
+            <li>
+              <NavLink to="/" end>
+                {t('header.navigation.main')}
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/about">{t('header.navigation.about')}</NavLink>
+            </li>
+            <li>
+              <NavLink to="/vzaemodia">
+                {t('header.navigation.interaction')}
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/chronicles">
+                {t('header.navigation.materials')}
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/service">{t('header.navigation.services')}</NavLink>
+            </li>
+            <li>
+              <NavLink to="/contact">{t('header.navigation.contacts')}</NavLink>
+            </li>
+            {isMobile && (
+              <li>{<a href="tel:+380502894133">+38 (050) 289-41-33</a>}</li>
+            )}
+          </ul>
+        </nav>
+      )}
     </div>
   );
 };
