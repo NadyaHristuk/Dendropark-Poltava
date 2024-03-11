e06202bc211d22b3eb6c7dc6dcd39009abe94501
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2024-03-11 22:10:52 +0200

    update

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,22 +1,36 @@
 import { AttentionVisitors } from '../components/AttentionVisitors/AttentionVisitors';
-// import s from './Home.module.scss';
 import Calendar from '../components/Calendar/Calendar';
 import ChroniclesList from '../components/Chronicles/ChroniclesList/ChroniclesList';
 import Container from '../components/Container/Container';
 import { HelpPark } from '../components/HelpPark/HelpPark/HelpPark';
 import Hero from '../components/Hero/Hero';
+import { Visual } from '../components/HelpPark/Visual/Visual';
 
 const HomePage = () => {
   return (
-    // <div className={s.container}>
-    <Container>
-      <Hero />
-      <AttentionVisitors />
-      <HelpPark />
-      <Calendar />
-      <ChroniclesList />
-    </Container>
-    // </div>
+    <>
+      <Container>
+        <Hero />
+      </Container>
+
+      <Container>
+        <AttentionVisitors />
+      </Container>
+
+      <Container>
+        <HelpPark />
+      </Container>
+
+      <Visual />
+
+      <Container>
+        <Calendar />
+      </Container>
+
+      <Container>
+        <ChroniclesList />
+      </Container>
+    </>
   );
 };
 
