856c0b16bdbbe05d2c63e5c05c02ec0161a222b3
sivlvy <vladislavsidenko@ukr.net>
2024-03-11 20:38:26 +0200

    update

--- src/components/Services/ServicesInfo/ServicesInfoMobile/ServicesInfoMobile.jsx
+++ src/components/Services/ServicesInfo/ServicesInfoMobile/ServicesInfoMobile.jsx
@@ -1,64 +1,21 @@
 import css from './services-info-mobile.module.scss';
 import { icons } from '../../../../assets';
-import { useState, useEffect } from 'react';
 import { usePagination } from '../../../../hooks/usePagination';
-import ServicePagination from '../ServicePagination';
 import data from '../../servicesInfo.json';
 import { useMedia } from '../../../../hooks/useMedia';
+import Slider from '../../../Slider/Slider';
 
-const ServiceInfoMobile = ({ items }) => {
-	const [pageSize, setPageSize] = useState(5); // Початковий розмір сторінки - 3
-
-	const [currentPage, setCurrentPage] = useState(1);
-
-	const [totalPages, setTotalPages] = useState(
-		Math.ceil(items.length / pageSize)
-	);
-
-	const { currentPosts, ...options } = usePagination({
-		postPerPage: 5,
+const ServiceInfoMobile = () => {
+	const { chunkedData } = usePagination({
+		perPage: 5,
 		data,
 	});
+	const { isMobile } = useMedia();
 
-	useEffect(() => {
-		function updatePageSize() {
-			const windowWidth = window.innerWidth;
-			if (windowWidth >= 960) {
-				setPageSize(5);
-			} else if (windowWidth >= 390) {
-				setPageSize(5);
-			}
-		}
-
-		updatePageSize();
-
-		window.addEventListener('resize', updatePageSize);
-
-		return () => {
-			window.removeEventListener('resize', updatePageSize);
-		};
-	}, []);
-
-	const currentItems = items.slice(
-		(currentPage - 1) * pageSize,
-		currentPage * pageSize
-	);
-
-	const handlePrevPage = () => {
-		if (currentPage > 1) {
-			setCurrentPage(currentPage - 1);
-		}
-	};
-
-	const handleNextPage = () => {
-		if (currentPage < totalPages) {
-			setCurrentPage(currentPage + 1);
-		}
-	};
-	return (
-		<div>
+	const list = (cards) => {
+		return (
 			<ul className={css.services_list}>
-				{currentItems.map(({ text, title, id }) => (
+				{cards.map(({ text, title, id }) => (
 					<li key={id} className={css.services_item}>
 						<div className={css.item_icon}>
 							<svg
@@ -77,13 +34,18 @@ const ServiceInfoMobile = ({ items }) => {
 					</li>
 				))}
 			</ul>
-			<ServicePagination
+		);
+	};
+	return (
+		<div>
+			{isMobile && <Slider chunkedData={chunkedData} currentPage={list} />}
+			{/* <ServicePagination
 				totalPages={totalPages}
 				setCurrentPage={setCurrentPage}
 				currentPage={currentPage}
 				goToNextPage={handleNextPage}
 				goToPrevPage={handlePrevPage}
-			/>
+			/> */}
 		</div>
 	);
 };
