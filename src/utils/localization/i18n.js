891427af91f2576030561776eaba5999fe62e49b
nahirniy <nahirniyy@gmail.com>
2024-03-12 22:18:47 +0200

    added switch language

--- src/utils/localization/i18n.js
+++ src/utils/localization/i18n.js
@@ -4,28 +4,30 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 import { ua, en } from './dictionaries';
 import { LANGUAGE_STORAGE_KEY } from '../../constants';
 
+const savedLanguage = localStorage.getItem(LANGUAGE_STORAGE_KEY) || 'ua';
+
 i18n
-  .use(LanguageDetector)
-  .use(initReactI18next)
-  .init({
-    detection: {
-      lookupLocalStorage: LANGUAGE_STORAGE_KEY,
-    },
-    lng: 'ua',
-    resources: { ua, en },
-    fallbackLng: 'ua',
-    debug: false,
-    returnObjects: true,
-    whitelist: ['ua', 'en'],
-    keySeparator: '.',
-    interpolation: {
-      escapeValue: false,
-      formatSeparator: ',',
-    },
-    react: {
-      useSuspense: true,
-      transSupportBasicHtmlNodes: true,
-    },
-  });
+	.use(LanguageDetector)
+	.use(initReactI18next)
+	.init({
+		detection: {
+			lookupLocalStorage: LANGUAGE_STORAGE_KEY,
+		},
+		lng: savedLanguage,
+		resources: { ua, en },
+		fallbackLng: 'ua',
+		debug: false,
+		returnObjects: true,
+		whitelist: ['ua', 'en'],
+		keySeparator: '.',
+		interpolation: {
+			escapeValue: false,
+			formatSeparator: ',',
+		},
+		react: {
+			useSuspense: true,
+			transSupportBasicHtmlNodes: true,
+		},
+	});
 
 export default i18n;
