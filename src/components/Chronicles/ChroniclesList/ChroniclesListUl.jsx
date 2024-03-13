fe26efd657acd7c08d7b743e08167544561a8f24
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2024-03-13 21:48:49 +0200

    resolve conflict

--- src/components/Chronicles/ChroniclesList/ChroniclesListUl.jsx
+++ src/components/Chronicles/ChroniclesList/ChroniclesListUl.jsx
@@ -4,11 +4,7 @@ import Slider from '../../Slider/Slider.jsx';
 import css from './ChroniclesList.module.scss';
 import chronicles from '../сhronicles';
 
-<<<<<<< Updated upstream
-export const ChroniclesListUl = () => {
-=======
 const ChroniclesListUl = () => {
->>>>>>> Stashed changes
 	const sortedChronicles = chronicles.sort((a, b) => b.date - a.date);
 	const { chunkedData } = usePagination({ perPage: 2, data: sortedChronicles });
 
@@ -16,15 +12,6 @@ const ChroniclesListUl = () => {
 		return (
 			<ul className={css.list}>
 				{cards.map(({ id, photo, title, description }) => (
-<<<<<<< Updated upstream
-					<ChroniclesItem
-						key={id}
-						id={id}
-						url={photo}
-						title={title}
-						description={description}
-					/>
-=======
 					<li key={id} className={css.item}>
 						<ChroniclesItem
 							id={id}
@@ -33,7 +20,6 @@ const ChroniclesListUl = () => {
 							description={description}
 						/>
 					</li>
->>>>>>> Stashed changes
 				))}
 			</ul>
 		);
@@ -41,8 +27,5 @@ const ChroniclesListUl = () => {
 
 	return <Slider currentPage={list} chunkedData={chunkedData} />;
 };
-<<<<<<< Updated upstream
-=======
 
 export default ChroniclesListUl;
->>>>>>> Stashed changes
