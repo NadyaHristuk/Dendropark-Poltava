c67961e80d1a00b7488d0c34afc28ae38a041783
KaterynaPryimakova <134732205+KaterynaPryimakova@users.noreply.github.com>
2024-03-13 20:50:32 +0200

    add hover/focus, add en dictionary, fix bugs

--- src/components/HelpPark/QrCode/QrCode.jsx
+++ src/components/HelpPark/QrCode/QrCode.jsx
@@ -4,25 +4,27 @@ import { useTranslation } from 'react-i18next';
 import icons from '/src/assets/icons/sprite.svg';
 
 export const QrCode = () => {
-  const { t } = useTranslation();
+	const { t } = useTranslation();
 
-  return (
-    <div className={css.desktopDiv}>
-      <p className={css.currentGoal}>{t('helpPark.currentGoal')}</p>
+	return (
+		<div className={css.desktopDiv}>
+			<p className={css.currentGoal}>{t('helpPark.currentGoal')}</p>
 
-      <div className={css.qrCodeWrap}>
-        <a className={css.qrCodeLink} href="/">
-          <svg className={css.qrCodeImg} width="196" height="195">
-            <use href={`${icons}#icon-qrCode`}></use>
-          </svg>
-        </a>
-      </div>
-      <a className={css.helpLink} href="/">
-        <svg className={css.heartIcon}>
-          <use href={`${icons}#icon-simplethankyou`}></use>
-        </svg>
-        {t('helpPark.button')}
-      </a>
-    </div>
-  );
+			<div className={css.qrCodeWrap}>
+				<svg className={css.qrCodeImg} width="196" height="195">
+					<use href={`${icons}#icon-qrCode`}></use>
+				</svg>
+			</div>
+			<a
+				className={css.helpLink}
+				href="https://prytulafoundation.org/"
+				target="_blank"
+			>
+				<svg className={css.heartIcon}>
+					<use href={`${icons}#icon-simplethankyou`}></use>
+				</svg>
+				{t('helpPark.button')}
+			</a>
+		</div>
+	);
 };
