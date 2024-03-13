89c20ef48f7269fa8985768605fda44a3ef5f179
Tkachenko_Yuriy <uvt86@ukr.net>
2024-03-13 19:09:49 +0200

    add fixed modal

--- src/components/Chronicles/ChroniclesItem/ChroniclesItem.jsx
+++ src/components/Chronicles/ChroniclesItem/ChroniclesItem.jsx
@@ -5,46 +5,48 @@ import { useMedia } from '../../../hooks/useMedia';
 import css from './ChroniclesItem.module.scss';
 
 const ChroniclesItem = ({ url, title, description }) => {
-  const [modalIsOpen, setIsOpen] = useState(false);
+	const [modalIsOpen, setIsOpen] = useState(false);
 
-  const { isMobile } = useMedia();
+	const { isMobile } = useMedia();
 
-  const shortDescription = description.slice(0, isMobile ? 80 : 300) + '...';
+	const shortDescription = description.slice(0, isMobile ? 80 : 300) + '...';
 
-  const openModal = () => {
-    setIsOpen(true);
-  };
+	const openModal = () => {
+		setIsOpen(true);
+		document.body.style.overflow = 'hidden';
+	};
 
-  const closeModal = () => {
-    setIsOpen(false);
-  };
+	const closeModal = () => {
+		setIsOpen(false);
+		document.body.style.overflow = '';
+	};
 
-  return (
-    <>
-      <div className={css.thumb}>
-        <img className={css.photo} src={url} alt={title} loading="lazy" />
-      </div>
-      <div className={css.info}>
-        <h4 className={css.title}>{title}</h4>
-        <p className={css.description}>{shortDescription}</p>
-        <button className={css.button} onClick={openModal}>
-          Читати далі{' '}
-          <svg className={css.arrowUpIcon}>
-            <use href={`${icons}#icon-arrow-up`}></use>
-          </svg>
-        </button>
-      </div>
-      {modalIsOpen && (
-        <ChroniclesModal
-          isOpen={modalIsOpen}
-          onRequestClose={closeModal}
-          url={url}
-          title={title}
-          description={description}
-        />
-      )}
-    </>
-  );
+	return (
+		<>
+			<div className={css.thumb}>
+				<img className={css.photo} src={url} alt={title} loading="lazy" />
+			</div>
+			<div className={css.info}>
+				<h4 className={css.title}>{title}</h4>
+				<p className={css.description}>{shortDescription}</p>
+				<button className={css.button} onClick={openModal}>
+					Читати далі{' '}
+					<svg className={css.arrowUpIcon}>
+						<use href={`${icons}#icon-arrow-up`}></use>
+					</svg>
+				</button>
+			</div>
+			{modalIsOpen && (
+				<ChroniclesModal
+					isOpen={modalIsOpen}
+					onRequestClose={closeModal}
+					url={url}
+					title={title}
+					description={description}
+				/>
+			)}
+		</>
+	);
 };
 
 export default ChroniclesItem;
