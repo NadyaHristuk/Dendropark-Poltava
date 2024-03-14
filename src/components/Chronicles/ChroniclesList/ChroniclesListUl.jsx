022cfeba18d777b2101f785a68327c48c981c0d1
Tkachenko_Yuriy <uvt86@ukr.net>
2024-03-14 13:01:47 +0200

    add default img

--- src/components/Chronicles/ChroniclesList/ChroniclesListUl.jsx
+++ src/components/Chronicles/ChroniclesList/ChroniclesListUl.jsx
@@ -2,16 +2,16 @@ import { usePagination } from '../../../hooks/usePagination.js';
 import ChroniclesItem from '../ChroniclesItem/ChroniclesItem';
 import Slider from '../../Slider/Slider.jsx';
 import css from './ChroniclesList.module.scss';
-import chronicles from '../сhronicles';
+// import chronicles from '../сhronicles';
 
-const ChroniclesListUl = () => {
-	const sortedChronicles = chronicles.sort((a, b) => b.date - a.date);
+const ChroniclesListUl = ({items}) => {
+	const sortedChronicles = items.sort((a, b) => b.date - a.date);
 	const { chunkedData } = usePagination({ perPage: 2, data: sortedChronicles });
 
-	const list = (cards) => {
+	const list = (items) => {
 		return (
 			<ul className={css.list}>
-				{cards.map(({ id, photo, title, description }) => (
+				{items.map(({ id, photo, title, description }) => (
 					<li key={id} className={css.item}>
 						<ChroniclesItem
 							id={id}
