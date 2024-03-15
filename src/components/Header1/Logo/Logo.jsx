b83a7215455aa014c237cb8ba23f9c86cb9a4936
BestStepInUa <beststep.in.ua@gmail.com>
2024-03-15 19:03:22 +0200

    fixed

--- src/components/Header1/Logo/Logo.jsx
+++ src/components/Header1/Logo/Logo.jsx
@@ -1,21 +1,21 @@
-import { Link } from "react-router-dom";
-import scss from "./Logo.module.scss";
-import { logo1x, logo2x } from "../../../assets";
+import { Link } from 'react-router-dom';
+import scss from './Logo.module.scss';
+import { logo1x, logo2x } from '../../../assets';
 
 const Logo = () => {
-    return (
-        <Link className={scss.link}>
-            <img
-                className={scss.icon}
-                src={logo1x}
-                srcSet={`${logo1x} 1x, ${logo2x} 2x`}
-                alt="Полтавський міський парк"
-            />
-            <p className={scss.title}>
-                Полтавський <span className={scss.titleColor}>міський</span> парк
-            </p>
-        </Link>
-    );
+	return (
+		<Link to="/" className={scss.link}>
+			<img
+				className={scss.icon}
+				src={logo1x}
+				srcSet={`${logo1x} 1x, ${logo2x} 2x`}
+				alt="Полтавський міський парк"
+			/>
+			<p className={scss.title}>
+				Полтавський <span className={scss.titleColor}>міський</span> парк
+			</p>
+		</Link>
+	);
 };
 
 export default Logo;
