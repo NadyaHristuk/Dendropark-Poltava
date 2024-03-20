e9e18edf3a63d3be6302e4f8e1bb6a2f1be5d355
Serhieie <89160714+Serhieie@users.noreply.github.com>
2024-03-20 13:17:30 +0200

    feat: back at vzaemodia

--- src/hooks/usePagination.js
+++ src/hooks/usePagination.js
@@ -2,9 +2,10 @@ export const usePagination = ({ perPage, data }) => {
   const chunkedData = [];
   const chunkSize = perPage;
 
-  for (let idx = 0; idx < data.length; idx += chunkSize) {
-    chunkedData.push(data.slice(idx, idx + chunkSize));
-  }
+  if (data)
+    for (let idx = 0; idx < data.length; idx += chunkSize) {
+      chunkedData.push(data.slice(idx, idx + chunkSize));
+    }
 
   return { chunkedData };
 };
