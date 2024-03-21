03c51a514c27530bc391a0d9c11ded2e9f693bbb
Serhieie <89160714+Serhieie@users.noreply.github.com>
2024-03-21 12:54:58 +0200

    styles_fix: pic height

--- src/components/Vzaemodia/VzaemodiaPage.jsx
+++ src/components/Vzaemodia/VzaemodiaPage.jsx
@@ -1,28 +1,28 @@
-import { TitleSection } from './TitleSection/TitleSection';
-import { TrialSection } from './TrialSection/TrialSection';
-import { OrganizatorsSection } from './OrganizatorsSection/OrganizatorsSection';
-import { RulesSection } from './RulesSection/RulesSection';
-import CalendarIframe from '../Calendar/CalendarIframe';
-import { SectionWrapper } from './SectionWrapper/SectionWrapper';
-import Map from '../Map/Map';
-import { ScrollToTop } from '../ScrollToTop/ScrollToTop';
+import { TitleSection } from "./TitleSection/TitleSection";
+import { TrialSection } from "./TrialSection/TrialSection";
+import { OrganizatorsSection } from "./OrganizatorsSection/OrganizatorsSection";
+import { RulesSection } from "./RulesSection/RulesSection";
+import CalendarIframe from "../Calendar/CalendarIframe";
+import { SectionWrapper } from "./SectionWrapper/SectionWrapper";
+import Map from "../Map/Map";
+import { ScrollToTop } from "../ScrollToTop/ScrollToTop";
 
 const VzaemodiaPage = () => {
-	return (
-		<>
-			<TitleSection />
-			<SectionWrapper>
-				<Map />
-			</SectionWrapper>
-			<TrialSection />
-			<OrganizatorsSection />
-			<SectionWrapper>
-				<CalendarIframe />
-			</SectionWrapper>
-			<RulesSection />
-			<ScrollToTop />
-		</>
-	);
+  return (
+    <>
+      <TitleSection />
+      <SectionWrapper>
+        <Map />
+      </SectionWrapper>
+      <TrialSection />
+      <OrganizatorsSection />
+      <SectionWrapper>
+        <CalendarIframe />
+      </SectionWrapper>
+      <RulesSection />
+      <ScrollToTop />
+    </>
+  );
 };
 
 export default VzaemodiaPage;
