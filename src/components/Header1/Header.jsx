046e3c8a407db35e850e2a94d71567fdc1ea6a96
Serhieie <89160714+Serhieie@users.noreply.github.com>
2024-04-05 16:26:15 +0300

    useNavigate for footer and header

--- src/components/Header1/Header.jsx
+++ src/components/Header1/Header.jsx
@@ -1,4 +1,5 @@
 import { useMedia } from "../../hooks/useMedia";
+import { useNavigate } from "react-router-dom";
 import Container from "../Container";
 import Logo from "./Logo";
 import Menu from "./Menu";
@@ -9,25 +10,30 @@ import scss from "./Header.module.scss";
 
 const Header = () => {
   const { isDesktop, isTablet } = useMedia();
+  const navigate = useNavigate();
+  const show = navigate.location === "/admin";
+
   return (
     <>
-      <div className={scss.headerWrapper}>
-        <header className={scss.header}>
-          <div className={scss.container}>
-            <div className={scss.wrapper}>
-              <div className={scss.leftWrapper}>
-                <Logo />
-                {isDesktop && <MenuList />}
-              </div>
-              <div className={scss.rightWrapper}>
-                {(isTablet || isDesktop) && <Phone />}
-                <LangSelect />
-                <Menu />
+      {show ? (
+        <div className={scss.headerWrapper}>
+          <header className={scss.header}>
+            <div className={scss.container}>
+              <div className={scss.wrapper}>
+                <div className={scss.leftWrapper}>
+                  <Logo />
+                  {isDesktop && <MenuList />}
+                </div>
+                <div className={scss.rightWrapper}>
+                  {(isTablet || isDesktop) && <Phone />}
+                  <LangSelect />
+                  <Menu />
+                </div>
               </div>
             </div>
-          </div>
-        </header>
-      </div>
+          </header>
+        </div>
+      ) : null}
     </>
   );
 };
